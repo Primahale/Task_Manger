fb2ef5d409b5cde6748afcf931fc1b5eee0cc682
Priyanka Mahale <97458322+Primahale@users.noreply.github.com>
2025-02-07 17:01:44 +0530

    Update Login.js

--- frontend_task/src/pages/Login.js
+++ frontend_task/src/pages/Login.js
@@ -5,7 +5,7 @@ import {
   Container, Card, CardContent, TextField, Button, Typography, Box, CircularProgress
 } from "@mui/material";
 
-const API_URL = "http://localhost:5000/api/auth/login"; // Change if needed
+const API_URL = "https://task-manger-yar3.onrender.com/api/auth/login"; // Change if needed
 
 function Login() {
   const [email, setEmail] = useState("");
